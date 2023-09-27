5e5e7e799973d70786acea17ba1a75d2a83bad02
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-26 21:17:43 -0300

    feat: add jump method, collect coin method, disable controls method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,95 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 3f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 25f;
+    [SerializeField] float jumpForce = 10f;
+
+    bool canMove = true;
+    bool isGrounded = false;
+
     Rigidbody2D rb;
+    SurfaceEffector2D surfaceEffector;
+    GameController gameController;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        surfaceEffector = FindAnyObjectByType<SurfaceEffector2D>();
+        gameController = FindAnyObjectByType<GameController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (canMove && gameController.gameStatus == GameStatus.Play)
+        {
+            RotatePlayer();
+            RespondToBoost();
+            RespondToJump();
+        }
+    }
+
+    void RespondToJump()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForceY(jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void RespondToBoost()
+    {
+        float y = Input.GetAxisRaw("Vertical");
+
+        if (y < 0 && surfaceEffector.speed != baseSpeed)
+        {
+            surfaceEffector.speed = baseSpeed;
+        }
+        else if (y > 0 && surfaceEffector.speed != boostSpeed)
+        {
+            surfaceEffector.speed = boostSpeed;
+        }
+        else if (y == 0)
+        {
+            surfaceEffector.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
         float x = Input.GetAxisRaw("Horizontal");
-        
         rb.AddTorque(torqueAmount * -x);
+    }
+
+    public void DisableControls()
+    {
+        canMove = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" && !isGrounded)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" && isGrounded)
+        {
+            isGrounded = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Coin")
+        {
+            gameController.GetCoin();
+            Destroy(collision.gameObject);
+        }
     }
 }
