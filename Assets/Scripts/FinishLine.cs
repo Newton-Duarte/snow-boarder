4c0c1d8857021752b4c8496b836fbb3356ffd22d
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-26 21:16:57 -0300

    feat: implement finish line script

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,7 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
+
     void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            finishEffect.Play();
+            GetComponent<AudioSource>().Play();
+            Invoke(nameof(ReloadScene), loadDelay);
+        }
+    }
+
+    void ReloadScene()
     {
         SceneManager.LoadScene(0);
     }
